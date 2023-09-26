6bd19c19c1bcc8ab86b3239880586984cfd1f408
CAMILA <77918638+CAMIILA0508@users.noreply.github.com>
2023-09-26 08:21:45 +0800

    Add files via upload

---
+++ BasicQueuingCashier/BasicQueuingCashier/QueuingForm.cs
@@ -0,0 +1,17 @@
+namespace BasicQueuingCashier {
+    public partial class QueuingForm : Form {
+        private CashierClass cashier;
+        public QueuingForm() {
+            InitializeComponent();
+            cashier = new CashierClass();
+            CashierWindowQueueForm cashierWindow = new CashierWindowQueueForm();
+            cashierWindow.Show();
+        }
+
+        private void btnCashier_Click(object sender, EventArgs e) {
+            lblQueue.Text = cashier.CashierGeneratedNumber("P - ");
+            CashierClass.getNumberInQueue = lblQueue.Text;
+            CashierClass.CashierQueue.Enqueue(CashierClass.getNumberInQueue);
+        }
+    }
+}
